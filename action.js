ac72a1da91660978b4d2439f6f0e0450143e7ad8
Mohit kumawat <mohitkumawat129@gmail.com>
2023-03-04 18:47:27 +0530

    Initilal commit

---
+++ action.js
@@ -0,0 +1,8 @@
+function custom() {
+  const searchBar = document.querySelector(".inputField");
+  const microPhone = document.querySelector(".inputField i:nth-of-type(2)");
+  // searchBar.style.width = "45vw";
+  // microPhone.classList.add("style2");
+  microPhone.style.display = "block";
+  searchBar.classList.add("style1");
+}
